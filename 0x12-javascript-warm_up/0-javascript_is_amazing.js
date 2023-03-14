3fe637d118b27643ca33ef6135b7575c4dec82a9
Adams <106625733+AdamsUthman@users.noreply.github.com>
2023-03-14 13:58:00 +0100

    Update 0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
-#!/usr/bin/mode
+#!/usr/bin/node
 
-//  Script that prints "Javascript is amazing".
+// Script that prints "JavaScript is amazing".
 
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
